47e0437f5723a2f4fab965af6527b47d8312a4fb
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s8d32.42wolfsburg.de>
2023-11-07 09:22:23 +0100

    Commit ex07 -Function that print digits from '0' to '9' in ascending order

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 09:17:56 by pabeckha          #+#    #+#             */
+/*   Updated: 2023/11/07 09:21:24 by pabeckha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    ft_putchar(char c);
+// void    ft_print_numbers(void);
+
+// int main(void)
+// {
+//     ft_print_numbers();
+//     return (0);
+// }
+
+void    ft_print_numbers(void)
+{
+    char digit;
+
+    digit = '0';
+    while(digit <= '9')
+    {
+        ft_putchar(digit);
+        digit++;
+    } 
+}
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
