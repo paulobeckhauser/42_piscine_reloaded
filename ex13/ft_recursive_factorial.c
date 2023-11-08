023acaae5e5fd55d63e4473e6f88e4fdd5d42232
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s13d66.42wolfsburg.de>
2023-11-08 13:28:36 +0100

    Correction of submission ex13 -Only submit the function, not main and others

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,24 +6,24 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 20:03:29 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/11/08 09:46:59 by pabeckha         ###   ########.fr       */
+/*   Updated: 2023/11/08 13:28:01 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int	ft_recursive_factorial(int nb);
+// int	ft_recursive_factorial(int nb);
 
-int	main(void)
-{
-	int	test;
-	int	factorial_test;
+// int	main(void)
+// {
+// 	int	test;
+// 	int	factorial_test;
 
-	test = 0;
-	factorial_test = ft_recursive_factorial(test);
-	printf("The factorial of %d(if < 13) is: %d\n", test, factorial_test);
-	return (0);
-}
+// 	test = 0;
+// 	factorial_test = ft_recursive_factorial(test);
+// 	printf("The factorial of %d(if < 13) is: %d\n", test, factorial_test);
+// 	return (0);
+// }
 
 int	ft_recursive_factorial(int nb)
 {
