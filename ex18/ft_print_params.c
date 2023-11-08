7b13a03e5270089258991fc41595ec68c5677c47
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s8d31.42wolfsburg.de>
2023-11-08 15:48:26 +0100

    Commit ex18 - print parameters of main

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 15:37:06 by pabeckha          #+#    #+#             */
+/*   Updated: 2023/11/08 15:47:50 by pabeckha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <unistd.h>
+
+void	ft_putchar(char c);
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+	while (argv[i])
+	{
+		while (argv[i][j])
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		j = 0;
+		i++;
+	}
+}
+
+// void	ft_putchar(char c)
+// {
+// 	write(1, &c, 1);
+// }
