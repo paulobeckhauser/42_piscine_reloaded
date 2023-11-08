51a83e3be055c955313e3148a79812a9051974e8
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s13d66.42wolfsburg.de>
2023-11-08 13:45:45 +0100

    Commit ex17 -reproduce function strcmp(string compare)

---
+++ ex17/ft_strcmp.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 13:21:28 by pabeckha          #+#    #+#             */
+/*   Updated: 2023/11/08 13:44:54 by pabeckha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+int ft_strcmp(char *s1, char *s2);
+
+int main(void)
+{
+    char *str1;
+    char *str2;
+    int diff_real;
+    int diff_built;
+
+    str1 = "b";
+    str2 = "a";
+    diff_real = strcmp(str1, str2);
+    diff_built = ft_strcmp(str1, str2);
+    printf("The value of using real function is: %d\n", diff_real);
+    printf("The value of using built function is: %d\n", diff_built);
+}
+
+int ft_strcmp(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    if ((*s1 || *s2) && (*s1 != *s2))
+    {
+        while(s1[i] == s2[i])
+        {
+            i++;
+        }
+        return (s1[i] - s2[i]);
+    }
+    else
+        return (0);
+}
