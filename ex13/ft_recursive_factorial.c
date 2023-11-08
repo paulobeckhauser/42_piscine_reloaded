920c6df4bda757f5f7afc36f6996ce48bbf96b77
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s8d31.42wolfsburg.de>
2023-11-08 09:54:44 +0100

    Commit ex13 -Recursive Factorial

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 20:03:29 by pabeckha          #+#    #+#             */
+/*   Updated: 2023/11/08 09:46:59 by pabeckha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb);
+
+int	main(void)
+{
+	int	test;
+	int	factorial_test;
+
+	test = 0;
+	factorial_test = ft_recursive_factorial(test);
+	printf("The factorial of %d(if < 13) is: %d\n", test, factorial_test);
+	return (0);
+}
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb == 1 | nb == 0)
+		return (1);
+	else if (nb >= 13 | nb < 0)
+		return (0);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
