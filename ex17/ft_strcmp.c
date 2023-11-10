f05f2699be11daea6a49d6783673c305d7a8ffc6
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s8d28.42wolfsburg.de>
2023-11-10 23:39:03 +0100

    Correction in function condition

--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -6,36 +6,36 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:21:28 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/11/08 15:32:53 by pabeckha         ###   ########.fr       */
+/*   Updated: 2023/11/10 23:37:53 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <string.h>
-// #include <stdio.h>
+#include <string.h>
+#include <stdio.h>
 
-// int ft_strcmp(char *s1, char *s2);
+int ft_strcmp(char *s1, char *s2);
 
-// int main(void)
-// {
-//     char *str1;
-//     char *str2;
-//     int diff_real;
-//     int diff_built;
+int main(void)
+{
+    char *str1;
+    char *str2;
+    int diff_real;
+    int diff_built;
 
-//     str1 = "Hello";
-//     str2 = "Hello!";
-//     diff_real = strcmp(str1, str2);
-//     diff_built = ft_strcmp(str1, str2);
-//     printf("The value of using real function is: %d\n", diff_real);
-//     printf("The value of using built function is: %d\n", diff_built);
-// }
+    str1 = "dasad";
+    str2 = "";
+    diff_real = strcmp(str1, str2);
+	printf("The value of using real function is: %d\n", diff_real);
+    diff_built = ft_strcmp(str1, str2);
+    printf("The value of using built function is: %d\n", diff_built);
+}
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
 
 	i = 0;
-	if ((*s1 || *s2) || (*s1 != *s2))
+	if (s1 != s2)
 	{
 		while (s1[i] == s2[i])
 		{
