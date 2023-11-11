b7168400dce41a460447c549fa6daa13ab10ba0d
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s14d75.42wolfsburg.de>
2023-11-11 18:12:01 +0100

    Correction, using #ifndef and #define

--- ex23/ft_point.h
+++ ex23/ft_point.h
@@ -6,14 +6,19 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 12:58:14 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/11/11 14:51:32 by pabeckha         ###   ########.fr       */
+/*   Updated: 2023/11/11 18:10:57 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct s_point	t_point;
+#ifndef	FT_POINT_H
+#define FT_POINT_H
+
+typedef struct s_point t_point;
 
 struct	s_point
 {
 	int	x;
 	int	y;
 };
+
+#endif
