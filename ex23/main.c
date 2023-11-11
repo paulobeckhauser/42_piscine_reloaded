77327ed7c69bf8e8ca3ed4ae6b1d7c5074901d10
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s14d75.42wolfsburg.de>
2023-11-11 14:52:36 +0100

    Commit ex23 - structure data type and .h file

---
+++ ex23/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 12:58:16 by pabeckha          #+#    #+#             */
+/*   Updated: 2023/11/11 14:47:00 by pabeckha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_point.h"
+#include <stdio.h>
+
+void	set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int	main(void)
+{
+	t_point	point;
+
+	set_point(&point);
+	printf("%d\n", point.x);
+	printf("%d\n", point.y);
+	return (0);
+}
