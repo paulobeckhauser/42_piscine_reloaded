3f946471803e8a55d88c3c996ff6a75407e66368
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s14d75.42wolfsburg.de>
2023-11-11 18:22:53 +0100

    Norminette corrections

--- ex22/ft_abs.h
+++ ex22/ft_abs.h
@@ -6,13 +6,13 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 11:38:59 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/11/11 18:07:46 by pabeckha         ###   ########.fr       */
+/*   Updated: 2023/11/11 18:21:56 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-# define ABS(Value) Value < 0 ? -Value : Value
+# define ABS(Value) ((Value < 0) ? -Value : Value)
 
 #endif
--- ex23/ft_point.h
+++ ex23/ft_point.h
@@ -6,19 +6,19 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 12:58:14 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/11/11 18:10:57 by pabeckha         ###   ########.fr       */
+/*   Updated: 2023/11/11 18:16:23 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	FT_POINT_H
-#define FT_POINT_H
+#ifndef FT_POINT_H
+# define FT_POINT_H
 
-typedef struct s_point t_point;
+typedef struct s_point	t_point;
 
-struct	s_point
+struct					s_point
 {
-	int	x;
-	int	y;
+	int					x;
+	int					y;
 };
 
 #endif
